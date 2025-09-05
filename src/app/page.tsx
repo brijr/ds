08d8c9cce7f47bbf910d46962b737da4ca03538f
Bridger Tower <bridgertower@gmail.com>
2025-09-05 13:17:51 +0200

    refactor Home component; introduce TypeWrapper for dynamic heading rendering and update structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,27 @@
 export default function Home() {
   return (
     <main className="p-8">
-      <h1>Hello World!</h1>
-      <p className="text-xl">This is some text!</p>
+      <TypeWrapper type="h1">
+        <p>This is some text!</p>
+      </TypeWrapper>
     </main>
   );
 }
+
+const TypeWrapper = ({
+  children,
+  type,
+}: {
+  children: React.ReactNode;
+  type: string;
+}) => {
+  return (
+    <div className="p-4 border">
+      {type === "h1" && <h1>{children}</h1>}
+      {type === "h2" && <h2>{children}</h2>}
+      {type === "h3" && <h3>{children}</h3>}
+      {type === "h4" && <h4>{children}</h4>}
+      {type === "h5" && <h5>{children}</h5>}
+    </div>
+  );
+};
