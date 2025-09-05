58e4d2734f3233f6586ba5facc5f3d9e826a8d33
Bridger Tower <bridgertower@gmail.com>
2025-09-05 13:01:47 +0200

    refactor Home component layout; simplify main structure and update typography styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="grid gap-4 p-8">
+    <main className="p-8">
       <h1>Hello World!</h1>
       <p className="text-xl">This is some text!</p>
     </main>
