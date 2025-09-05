c9970bfdd8f259bfd4bcb1f53d86f1c6a0976f25
Bridger Tower <bridgertower@gmail.com>
2025-09-05 13:37:25 +0200

    wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,55 @@
+"use client";
+
+import { useState } from "react";
+
 const text = `
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
 `;
 
-const elementTypes = ["h1", "h2", "h3", "h4", "h5", "h6", "p", "a"];
+const elementTypes = [
+  "h1",
+  "h2",
+  "h3",
+  "h4",
+  "h5",
+  "h6",
+  "p",
+  "a",
+  "blockquote",
+  "code",
+  "pre",
+  "em",
+  "strong",
+  "small",
+  "mark",
+  "del",
+  "ins",
+  "sub",
+  "sup",
+];
 
 export default function Home() {
+  const [demoText, setDemoText] = useState(text);
+
   return (
-    <main className="p-2 space-y-4">
-      {elementTypes.map((type) => (
-        <Wrapper key={type} as={type as React.ElementType}>
-          {text}
-        </Wrapper>
-      ))}
+    <main className="divide-y divide-border">
+      <div className="grid gap-1 p-4">
+        <label className="text-sm text-muted-foreground">Demo Text</label>
+        <input
+          className="w-full border border-border p-2 bg-muted"
+          type="text"
+          value={demoText}
+          onChange={(e) => setDemoText(e.target.value)}
+        />
+      </div>
+
+      <div className="p-4 space-y-4">
+        {elementTypes.map((type) => (
+          <Wrapper key={type} as={type as React.ElementType}>
+            {demoText}
+          </Wrapper>
+        ))}
+      </div>
     </main>
   );
 }
